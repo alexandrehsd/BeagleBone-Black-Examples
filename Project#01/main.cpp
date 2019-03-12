e14286847d1ad4b1af0e7b10884e7337070279a5
Alexandre Henrique <alexandre.hsd@gmail.com>
2019-03-12 07:33:11 -0300

    Script for pushbutton

--- Project#01/main.cpp
+++ Project#01/main.cpp
@@ -8,53 +8,43 @@
 #include <string.h>
 
 using std::cout;
-
-/*
-void PrintValues(){
-	cout << "\n-------- STATES --------\n";
-	cout << "adc value = " << adc.getIntValue() << endl;
-	cout << "pwm duty cycle = " << getDutyCycle() << endl;
-}
-*/
+using namespace BlackLib;
 
 int main(int argc, char * argv[]){
-	//time_t real_time;
-
 	// analog pin 3 for reading and writing (r & w)
 	ADC adc(AINx::AIN3); 
 
 	// instantiating pwm pin P9_22 for r & w
-	PWM pwm(P9_22); 
+	PWM pwm(P9_21); 
 
 	// Initializing pwm
-	pwm.setState(run); 
+	pwm.setState(statePwm::run); 
 	
 	// Digital input and output
-	// BlackGPIO pushbutton(GPIO_15, input);
-	// BlackGPIO led(GPIO_50, output);
+	BlackGPIO pushbutton(GPIO_15, input);
+	BlackGPIO led(GPIO_50, output);
 
 	float dutyCycle;
-	int period = 1000000000;
+	int period = 1000000;
+	pwm.setPeriod(period);
 
 	while(true){
-		/* while(!strcmp(pushbutton.getValue(), "0")){
+		string val = pushbutton.getValue();
+
+		if (val == "1"){
+			led.setValue(high);
+
+			dutyCycle  = adc.getPercentValue();
+			cout << "adc value = " << adc.getIntValue() << " ";
+
+			pwm.setDutyCycle(dutyCycle*period/100.0);
+			cout << "pwm duty cycle = " << pwm.getDutyCycle() << " ";
+			cout << "division = " << dutyCycle*period/100.0 << endl;
+		}
+		else {
 			pwm.setDutyCycle(0);
 			led.setValue(low);
-		} */
-
-		//real_time1 = time( (time_t *) 0);
-
-		// led.setValue(high);
-
-		dutyCycle  = adc.getPercentValue();
-		// cout << "adc value = " << adc.getIntValue() << endl;
-		pwm.setPeriod(period);
-		pwm.setDutyCycle(dutyCycle*period/100.0);
-		// cout << "pwm duty cycle = " << getDutyCycle() << endl;
-
-		//real_time2 = time( (time_t *) 0);
-		//if ((real_time2-real_time1) >= 3)
-		//	PrintValues();
+		}
 	}
 	return 0;
 }
