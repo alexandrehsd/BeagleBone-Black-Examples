e1c6729fdf46fefd5fe55597a58afeb6a8937ad2
Alexandre Henrique <alexandre.hsd@gmail.com>
2019-04-01 18:42:18 -0300

    Syntax error correction

--- Project#02/main.cpp
+++ Project#02/main.cpp
@@ -15,53 +15,60 @@ Os leds devem piscar 6 vezes, entre intervalos de 0.4 segundos
 #define NBLINKS 6
 #define NRESPONSES 6
 #define TIME2ANS 10 // seconds
+#define NDIG_OUTPUT 3
+#define NDIG_INPUT 3
 
 using namespace BlackLib;
 
-void call_turn();
-void press_inputs();
-bool compare();
-int score(int score_counter);
+void call_turn(BlackGPIO leds[], int output[]);
+void press_inputs(BlackGPIO buttons[], int input[]);
+bool compare(int input[], int output[]);
+int score(int score_counter, int input[], int output[]);
 
 int main(int argc, char *argv[]){
 	// Digital outputs
-	BlackGPIO led_0(GPIO_67, output);
-	BlackGPIO led_1(GPIO_68, output);
-	BlackGPIO led_2(GPIO_69, output);
+	BlackGPIO led_0(GPIO_35, output);
+	BlackGPIO led_1(GPIO_39, output);
+	BlackGPIO led_2(GPIO_38, output);
 
-	BlackGPIO led_state_ready(GPIO_70, output);
+	BlackGPIO led_state_ready(GPIO_34, output);
 
 	// Digital inputs
-	BlackGPIO button_0(GPIO_7, input);
-	BlackGPIO button_1(GPIO_8, input);
-	BlackGPIO button_2(GPIO_6, input);
+	BlackGPIO button_0(GPIO_33, input);
+	BlackGPIO button_1(GPIO_37, input);
+	BlackGPIO button_2(GPIO_63, input);
 
 
-	static BlackGPIO leds[3] = {led_0, led_1, led_2};
-	static BlackGPIO buttons[3] = {button_0, button_1, button_2};
+	BlackGPIO leds[3] = {led_0, led_1, led_2};
+	BlackGPIO buttons[3] = {button_0, button_1, button_2};
 
-  	static int output[NBLINKS];
-  	static int input[NRESPONSES];
+  	int output[NBLINKS];
+  	int input[NRESPONSES];
   	
   	int score_counter = 5;
 
   	while(score_counter > 0 && score_counter < 10){
-  		call_turn();
+  		call_turn(leds, output);
 
   		sleep(4);
 
   		led_state_ready.setValue(high);
-  		press_inputs();
+  		press_inputs(buttons, input);
   		led_state_ready.setValue(low);
 
-  		score_counter = score();
+  		score_counter = score(score_counter, input, output);
   		printf("Your current score is: %d\n", score_counter);
   	}
+
+  	if(score_counter == 10) 
+  		printf("%s\n", "CONGRATULATIONS, YOU WON!");
+  	else 
+  		printf("%s\n", "YOU LOSE!");
 
 	exit(0);
 }
 
-void call_turn(){
+void call_turn(BlackGPIO leds[], int output[]){
 	/* initialize random seed: */
   	srand (time(NULL));
 
@@ -71,21 +78,21 @@ void call_turn(){
   	for(i_rand = 0; i_rand<NBLINKS; i_rand++){
   		output[i_rand] = rand() % 3;
 
-  		for(j_led = 0; j_led<3; j_led++)
+  		for(j_led = 0; j_led<NDIG_OUTPUT; j_led++)
   			leds[i_rand].setValue(low); // Consider put this after each usleep
 
   		switch(output[i_rand]){
   			case 0:
   				leds[i_rand].setValue(high);
-  				usleep(400);
+  				usleep(400000);
   				break;
   			case 1:
   				leds[i_rand].setValue(high);
-  				usleep(400);
+  				usleep(400000);
   				break;
   			case 2:
   				leds[i_rand].setValue(high);
-  				usleep(400);
+  				usleep(400000);
   				break;
   			default:
   				printf("%s\n", "Wrong assignement for a digital output!");
@@ -94,7 +101,7 @@ void call_turn(){
   	}
 }
 
-void press_inputs(){
+void press_inputs(BlackGPIO buttons[], int input[]){
 	int response_counter = 0;
 	int i;
 
@@ -106,7 +113,7 @@ void press_inputs(){
 
 	while(response_counter < NRESPONSES){
 
-		for (i = 0; i < 3; i++)	{
+		for (i = 0; i < NDIG_INPUT; i++)	{
 			strs_b[i] = buttons[i].getValue();
 
 			if(strs_b[i] == "1"){
@@ -125,8 +132,8 @@ void press_inputs(){
 	}
 }
 
-bool compare(){
-	int i
+bool compare(int input[], int output[]){
+	int i;
 	for (i = 0; i < NRESPONSES; i++){
 		if (input[i] != output[i]){
 			return false;
@@ -135,6 +142,6 @@ bool compare(){
 	return true;
 }
 
-int score(int score_counter){
-	return compare() ? score_counter+1 ; score_counter-1;
+int score(int score_counter, int input[], int output[]){
+	return compare(input, output) ? score_counter+1 : score_counter-1;
 }
