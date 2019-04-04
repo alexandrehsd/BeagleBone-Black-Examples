31f824ab1f6ff49e70917de18c573945994369d8
Alexandre Henrique <alexandre.hsd@gmail.com>
2019-04-04 07:36:58 -0300

    mapping correct GPIOs into 7seg leds

--- Project#02/display_7segments.cpp
+++ Project#02/display_7segments.cpp
@@ -5,27 +5,26 @@
 #include "PWM/PWM.h"
 #include "ADC/Adc.h"
 
+using namespace BlackLib;
 
 int main(int argc, char *argv[]){
 	
-	BlackGPIO displayLed_a(GPIO_73, output); // a
-	BlackGPIO displayLed_b(GPIO_75, output); // b
-	BlackGPIO displayLed_c(GPIO_77, output); // c
-	BlackGPIO displayLed_d(GPIO_44, output); // d
-	BlackGPIO displayLed_e(GPIO_68, output); // e
-	BlackGPIO displayLed_f(GPIO_35, output); // f
-	BlackGPIO displayLed_g(GPIO_67, output); // g
-	BlackGPIO displayLed_dp(GPIO_87, output); // DP
+	BlackGPIO disp_uc(GPIO_4, output); // upper centenr
+	BlackGPIO disp_ur(GPIO_31, output); // upper right
+	BlackGPIO disp_ul(GPIO_117, output); // upper left
+	BlackGPIO disp_mi(GPIO_20, output); // mid
+	BlackGPIO disp_lr(GPIO_48, output); // lower right
+	BlackGPIO disp_ll(GPIO_14, output); // lower left
+	BlackGPIO disp_lc(GPIO_5, output); // lower center
 
 	while(true){
-		displayLed_a.setValue(high); // a
-		displayLed_b.setValue(high); // b
-		displayLed_c.setValue(high); // c
-		displayLed_d.setValue(high); // d
-		displayLed_e.setValue(high); // e
-		displayLed_f.setValue(high); // f
-		displayLed_g.setValue(high); // g
-		displayLed_dp.setValue(high); // DP
+		disp_uc.setValue(high); 
+		disp_ur.setValue(high); 
+		disp_ul.setValue(high); 
+		disp_mi.setValue(high); 
+		disp_lr.setValue(high); 
+		disp_ll.setValue(high); 
+		disp_lc.setValue(high); 
 	}
 	
 
