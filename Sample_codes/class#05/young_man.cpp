9825bf3f3daaa74425c79c2c2114609a7d67f8ba
Alexandre Henrique <alexandre.hsd@gmail.com>
2019-04-16 07:20:56 -0300

    Priorities

---
+++ Sample_codes/class#05/young_man.cpp
@@ -0,0 +1,23 @@
+#include<sys/types.h>
+#include<signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h> // system()
+#include <math.h>
+
+#include <sys/time.h>     // getpriority(int which, int who)  setpriority(int which, int who, int prio);
+#include <sys/resource.h>
+
+using std::cout;
+
+int main(){
+    cpu_set_t set;
+	CPU_ZERO(&set);        // clear cpu mask
+	CPU_SET(0, &set);      // set cpu 0
+	sched_setaffinity(getpid(), sizeof(cpu_set_t), &set);  // 0 is the calling process
+
+    setpriority(PRIO_PROCESS, 0, 10);
+
+    while(1);
+}
